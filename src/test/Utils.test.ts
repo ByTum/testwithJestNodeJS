5dec92e4e8eb09284ee77bfd8ad08a333c1f5e70
Tum <tum-ok@msn.com>
2024-07-23 13:42:41 +0700

    Structure of an unit test

--- src/test/Utils.test.ts
+++ src/test/Utils.test.ts
@@ -1,7 +1,19 @@
+// AAA principle
+// 1. arrange
+// 2. act
+// 3. assert
+
 import { toUpperCase } from '../app/Utils';
 describe('Utils Test Suite', () => {
-  test('should return upppercase', () => {
-    const result = toUpperCase('abc');
-    expect(result).toBe('ABC');
+  it('should return upppercase of valid string', () => {
+    // arrange
+    const sut = toUpperCase;
+    const expected = 'ABC';
+
+    // act
+    const actual = sut('abc');
+
+    // assert
+    expect(actual).toBe(expected);
   });
 });
