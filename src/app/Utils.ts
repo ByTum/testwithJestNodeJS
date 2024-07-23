839989eaab5b5de137717e67d11d74afe0a0497c
Tum <tum-ok@msn.com>
2024-07-23 14:19:54 +0700

    Jest assertions and matchers

--- src/app/Utils.ts
+++ src/app/Utils.ts
@@ -1,3 +1,21 @@
 export function toUpperCase(arg: string) {
   return arg.toUpperCase();
 }
+
+export type stringInfo = {
+  lowerCase: string;
+  uppperCase: string;
+  characters: string[];
+  length: number;
+  extraInfo: Object | undefined;
+};
+
+export function getStringInfo(arg: string): stringInfo {
+  return {
+    lowerCase: arg.toLowerCase(),
+    uppperCase: arg.toUpperCase(),
+    characters: Array.from(arg),
+    length: arg.length,
+    extraInfo: {},
+  };
+}
--- src/test/Utils.test.ts
+++ src/test/Utils.test.ts
@@ -3,7 +3,7 @@
 // 2. act
 // 3. assert
 
-import { toUpperCase } from '../app/Utils';
+import { getStringInfo, toUpperCase } from '../app/Utils';
 describe('Utils Test Suite', () => {
   it('should return upppercase of valid string', () => {
     // arrange
@@ -15,5 +15,40 @@ describe('Utils Test Suite', () => {
 
     // assert
     expect(actual).toBe(expected);
+  });
+
+  // .only jest run only this test
+  it.only('should return info for valid string', () => {
+    const actual = getStringInfo('My-String');
+
+    // comparing primitives value use toBe
+    expect(actual.lowerCase).toBe('my-string');
+
+    // compare Object use toEqual
+    expect(actual.extraInfo).toEqual({});
+
+    expect(actual.characters.length).toBe(9);
+    expect(actual.characters).toHaveLength(9);
+
+    expect(actual.characters).toEqual([
+      'M',
+      'y',
+      '-',
+      'S',
+      't',
+      'r',
+      'i',
+      'n',
+      'g',
+    ]);
+    expect(actual.characters).toContain<string>('M');
+    expect(actual.characters).toEqual(
+      expect.arrayContaining(['S', 't', 'r', 'i', 'n', 'g', 'M', 'y', '-'])
+    );
+
+    expect(actual.extraInfo).not.toBe(undefined);
+    expect(actual.extraInfo).not.toBeUndefined();
+    expect(actual.extraInfo).toBeDefined();
+    expect(actual.extraInfo).toBeTruthy();
   });
 });
