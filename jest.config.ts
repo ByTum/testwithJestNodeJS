be7394c5257b94bc6712331d8a574aa483e231ce
Tum <tum-ok@msn.com>
2024-07-25 15:10:20 +0700

    PassChecker setup | Learn TDD Test driven development

--- jest.config.ts
+++ jest.config.ts
@@ -1,11 +1,16 @@
 import type { Config } from '@jest/types';
 
+const baseDir = '<rootDir>/src/app/pass_checker';
+const baseTestDir = '<rootDir>/src/test/pass_checker';
+
 const config: Config.InitialOptions = {
   preset: 'ts-jest', // for jest understand typescript
   testEnvironment: 'node',
   verbose: true, // show more info in the console
   collectCoverage: true,
-  collectCoverageFrom: ['<rootDir>/src/app/**/*.ts'], // all file in the root directory .ts
+  // collectCoverageFrom: ['<rootDir>/src/app/**/*.ts'], // all file in the root directory .ts
+  collectCoverageFrom: [`${baseDir}/**/*.ts`],
+  testMatch: [`${baseTestDir}/**/*.ts`],
 };
 
 export default config;
